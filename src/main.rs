ee5ed5085058869407fc28a8a5a7b0e576265fdd
Shota Mishima <sm4mma@gmail.com>
2021-11-27 14:32:23 +0000

    Add FileReader

--- src/main.rs
+++ src/main.rs
@@ -22,10 +22,19 @@ struct Opts {
 fn main() {
     let opts = Opts::parse();
 
-    match opts.formula_file {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified."),
-    }
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
 
-    println!("Is verbosity specified?: {}", opts.verbose);
+    } else {
+        println!("No file is specified.");
+    }
+}
+
+fn run(reader: BufReader<File>, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
