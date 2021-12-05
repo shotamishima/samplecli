4c39583f434f955545664e8c36172e4aeb491d8b
Shota Mishima <sm4mma@gmail.com>
2021-12-05 04:10:03 +0000

    Update error handling at main.rs.

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,26 @@ impl RpnCalculator {
         Self(verbose)
     } 
 
-    pub fn eval(&self, formula: &str) -> i32 {
+    pub fn eval(&self, formula: &str) -> Result<i32> {
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         self.eval_inner(&mut tokens)
     }
 
     // eval_innerの中でstackの中身をいじるので、可変な参照
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
         let mut stack = Vec::new();
+        let mut pos = 0;
 
         while let Some(token) = tokens.pop() {
+            pos += 1;
+
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x);
             } else {
                 // 最後にpushした数値、数値出なかったらerror出力
-                let y = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
                 // その前にpushした数値
-                let x = stack.pop().expect("invalid syntax");
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
 
                 // 今のtoken、記号の場合はx,yを使って計算する
                 let res = match token {
@@ -53,35 +56,35 @@ impl RpnCalculator {
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("invalid token"),
+                    _ => bail!("invalid token at {}", pos),
                 };
                 stack.push(res);
             }
+            // -v オプションが設定されている場合、この時点でのtoken, stackをプリントする
             if self.0 {
-                println!("{:?} {:?}", token, stack);
+                println!("{:?} {:?}", tokens, stack);
             }
         }
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
+        // stackを処理した結果、stackの中身は数字１文字になるはず。そうでない場合はinvalid
+        // suntaxをエラー出力　
+        ensure!(stack.len() == 1, "invalid syntax");
+        Ok(stack[0])
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
     let opts = Opts::parse();
 
     if let Some(path) = opts.formula_file {
-        let f = File::open(path).unwrap();
+        let f = File::open(path)?;
         let reader = BufReader::new(f);
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
 
     } else {
         // println!("No file is specified.");
         let stdin = stdin();
         let reader = stdin.lock();
-        run(reader, opts.verbose);
+        run(reader, opts.verbose)
     }
 }
 
@@ -106,12 +109,12 @@ mod tests {
     #[test]
     fn test_ok() {
         let calc = RpnCalculator::new(false);
-        assert_eq!(calc.eval("5"), 5);
-        assert_eq!(calc.eval("50"), 50);
-        assert_eq!(calc.eval("-50"), -50);
+        assert_eq!(calc.eval("5").unwrap(), 5);
+        assert_eq!(calc.eval("50").unwrap(), 50);
+        assert_eq!(calc.eval("-50").unwrap(), -50);
 
-        assert_eq!(calc.eval("2 3 +"), 5);
-        assert_eq!(calc.eval("2 3 *"), 6);
+        assert_eq!(calc.eval("2 3 +").unwrap(), 5);
+        assert_eq!(calc.eval("2 3 *").unwrap(), 6);
     }
 
     #[test]
